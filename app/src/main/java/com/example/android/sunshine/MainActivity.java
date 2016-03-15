1a454c7a4a4f9ae8c0acd18e8caee1eeef807579
apg211 <apg211@gmail.com>
2016-03-15 12:38:55 +0530

    Changes Before Hierarchy Viewer

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -11,8 +11,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-;
-
 public class MainActivity extends AppCompatActivity {
 
         private String mLocation;
